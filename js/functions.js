8b1bcc97b9cf3c46bbd3d93e4f580ef3a5d2fa18
Krishna Tiwari <krishnatiwaridtea@gmail.com>
2022-02-12 13:53:12 +0530

    completed js code

--- js/functions.js
+++ js/functions.js
@@ -1,30 +1,32 @@
-const generateSampleData = (classesNo, subjects, days) => {
+const generateSampleData = (classesNo, days) => {
   let result = [];
   for (let i = 0; i < classesNo; i++) {
     const classData = {
       id: i,
       name: `Class ${i + 1}`,
-      schedule: sampleScheduleForClass(subjects, days),
+      schedule: sampleScheduleForClass(days),
     };
     result.push(classData);
   }
   return result;
 };
 
-const sampleScheduleForClass = (subjects, days) => {
+const sampleScheduleForClass = (days) => {
   let result = {};
-  for (let i = 0; i < Object.keys(subjects).length; i++) {
+  for (let i = 0; i < days.length; i++) {
     result[days[i]] = [];
   }
   return result;
 };
 
-const checks = (day, subject, subjects, classes, period, dayName) => {
+const checks = (schedule, day, subject, subjects, classes, period, dayName) => {
   if (
-    checkForSubjectAlreadyInDay(day, subject) &&
-    !checkForSubjectInAnyClass(subject, classes, period, dayName)
+    checkForSubjectInAnyClass(subject, classes, period, dayName) ||
+    checkForSubjectAlreadyInDay(day, subject) >= 2 ||
+    checkForSubjectOccuredMaxTimes(subject, schedule) >= subjects[subject]
   ) {
     return checks(
+      schedule,
       day,
       randomSubject(subjects),
       subjects,
@@ -38,7 +40,7 @@ const checks = (day, subject, subjects, classes, period, dayName) => {
 };
 
 const checkForSubjectAlreadyInDay = (day, subject) => {
-  return day.includes(subject);
+  return day.filter((item) => item == subject).length;
 };
 
 const checkForSubjectInAnyClass = (subject, classes, period, day) => {
@@ -50,11 +52,102 @@ const checkForSubjectInAnyClass = (subject, classes, period, day) => {
   return false;
 };
 
+const checkForSubjectOccuredMaxTimes = (subject, schedule) => {
+  let count = 0;
+  for (let i = 0; i < Object.keys(schedule).length; i++) {
+    const day = schedule[Object.keys(schedule)[i]];
+    if (day.includes(subject)) {
+      count++;
+    }
+  }
+
+  //   console.log(
+  //     Object.entries(schedule)
+  //       .flat()
+  //       .flat()
+  //       .filter((item) => item == subject).length,
+  //     subject
+  //   );
+  return Object.entries(schedule)
+    .flat()
+    .flat()
+    .filter((item) => item == subject).length;
+};
+
 const randomSubject = (subjects) => {
   const keys = Object.keys(subjects);
   const randomIndex = Math.floor(Math.random() * keys.length);
   return keys[randomIndex];
 };
+
+// console.log(
+//   checkForSubjectOccuredMaxTimes(
+//     "Biology",
+//     {
+//       Monday: [
+//         "SST",
+//         "Math",
+//         "Biology",
+//         "English",
+//         "Hindi",
+//         "Physics",
+//         "Chemistry",
+//         "Games",
+//       ],
+//       Tuesday: [
+//         "Games",
+//         "Chemistry",
+//         "Biology",
+//         "Math",
+//         "Hindi",
+//         "Physics",
+//         "English",
+//         "SST",
+//       ],
+//       Wednesday: [
+//         "Biology",
+//         "Games",
+//         "Chemistry",
+//         "English",
+//         "Hindi",
+//         "Math",
+//         "SST",
+//         "Physics",
+//       ],
+//       Thursday: [
+//         "Math",
+//         "SST",
+//         "Hindi",
+//         "Chemistry",
+//         "Biology",
+//         "Games",
+//         "Physics",
+//         "English",
+//       ],
+//       Friday: [
+//         "English",
+//         "Chemistry",
+//         "Biology",
+//         "Hindi",
+//         "Math",
+//         "Games",
+//         "Physics",
+//         "SST",
+//       ],
+//       Saturday: [
+//         "English",
+//         "SST",
+//         "Biology",
+//         "Math",
+//         "Hindi",
+//         "Games",
+//         "Chemistry",
+//         "Physics",
+//       ],
+//     },
+//     6
+//   )
+// );
 
 module.exports = {
   generateSampleData,
--- js/index.js
+++ js/index.js
@@ -1,26 +1,30 @@
 const { generateSampleData, randomSubject, checks } = require("./functions.js");
 const fs = require("fs");
 
+console.log(
+  "---------------------------------------------------------------------------------------------------"
+);
 const createScheduleForSchool = (classesNo, subjects, periods, days) => {
-  let result = generateSampleData(classesNo, subjects, days);
+  let result = generateSampleData(classesNo, days);
 
   for (let i = 0; i < result.length; i++) {
     console.log("-----", result[i].name);
     const element = result[i];
 
     for (let j = 0; j < days.length; j++) {
-      console.log("-----", days[j]);
+      console.log("---", days[j]);
       for (let k = 0; k < periods; k++) {
-        element.schedule[days[j]].push(
-          checks(
-            element.schedule[days[j]],
-            randomSubject(subjects),
-            subjects,
-            result.filter((item) => item.id !== element.id),
-            k,
-            days[j]
-          )
+        let period = checks(
+          element.schedule,
+          element.schedule[days[j]],
+          randomSubject(subjects),
+          subjects,
+          result.filter((item) => item.id !== element.id),
+          k,
+          days[j]
         );
+        console.log(period);
+        element.schedule[days[j]].push(period);
       }
     }
   }
@@ -31,19 +35,31 @@ const createScheduleForSchool = (classesNo, subjects, periods, days) => {
 let out = createScheduleForSchool(
   1,
   {
-    Math: 10,
-    English: 7,
+    Math: 7,
+    English: 6,
     SST: 7,
-    Physics: 5,
+    Physics: 7,
     Hindi: 5,
     Games: 2,
-    Chemistry: 6,
-    Biology: 6,
+    Chemistry: 7,
+    Biology: 7,
   },
   8,
   ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 );
 
-// console.log(out);
+let obj = {
+  Monday: [
+    "SST",
+    "Math",
+    "Biology",
+    "English",
+    "Hindi",
+    "Physics",
+    "Chemistry",
+    "Games",
+  ],
+  Tuesday: ["Games", "Chemistry", "Biology", "Math", "Hindi", "Physics"],
+};
 
 fs.writeFileSync("./output.js", JSON.stringify(out));
